9b92d45f41b2df8933c32f0bb3e8479cede4632f
Juherles <juhe2512@gmail.com>
2025-08-30 01:25:49 +0000

    git push origin main

--- Estructuras/double/ejemplo_double.c
+++ Estructuras/double/ejemplo_double.c
@@ -5,5 +5,4 @@ int main() { // Función principal
     double y = 2.7182818284; // Declara una variable double 'y' y le asigna un valor
     double suma = x + y; // Declara una variable 'suma' y almacena la suma de 'x' y 'y'
     printf("La suma de %.10f y %.10f es %.10f\n", x, y, suma); // Imprime el resultado en pantalla
-    return 0; // Indica que el programa terminó correctamente
-}
+    return 0;} // Indica que el programa terminó correctamente
